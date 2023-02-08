36a126ed06722c2add11b45d8abb9d15f75ffaf2
hyunsang-ahn <happylovetkd@naver.com>
2023-02-08 22:15:42 +0000

    mongodb connet fail

--- server/db.js
+++ server/db.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 module.exports = () => {
     mongoose.set('strictQuery', false);
     function connect() {
-      mongoose.connect('localhost:27017', function(err) {
-        if (err) {
-          console.error('mongodb connection error', err);
-        }
-        console.log('mongodb connected');
-      });
+      mongoose.connect('mongodb://127.0.0.1:27017');
+
     }
     connect();
     
-    mongoose.connection.on('disconnected', connect);
+    // mongoose.connection.on('disconnected', connect);
   };
